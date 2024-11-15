7d254cf9f996c326c3d8403c2b05f76dd15f5ce3
sadrack <gabrielscr2023@gmail.com>
2024-11-15 14:14:18 -0300

    criado e configurado a base do projeto

---
+++ src/main/java/com/sarlym/osmanager/OsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sarlym.osmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarlym/osmanager/OsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarlym.osmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsManagerApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
