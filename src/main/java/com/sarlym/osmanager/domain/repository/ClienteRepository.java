a642afa8ad6c40d5a20dcc4412713658e7b392b8
sadrack <gabrielscr2023@gmail.com>
2024-11-17 14:55:49 -0300

    feat: adicionado o endpoint de listar todos os clientes

---
+++ src/main/java/com/sarlym/osmanager/domain/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.sarlym.osmanager.domain.repository;
+
+import com.sarlym.osmanager.domain.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
