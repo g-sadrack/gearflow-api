d224de8e5ab8038deb45a66c18d3db70f214c214
sadrack404 <gabrielscr2023@gmail.com>
2024-11-19 16:52:38 -0300

    feat: criado o dto e endpoint de criacao do cliente

--- src/main/java/com/sarlym/osmanager/domain/service/ClienteService.java
+++ src/main/java/com/sarlym/osmanager/domain/service/ClienteService.java
@@ -1,5 +1,7 @@
 package com.sarlym.osmanager.domain.service;
 
+import com.sarlym.osmanager.api.dto.request.ClienteRequest;
+import com.sarlym.osmanager.api.responseconverter.ClienteConverter;
 import com.sarlym.osmanager.domain.model.Cliente;
 import com.sarlym.osmanager.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +11,21 @@ import java.util.List;
 
 @Service
 public class ClienteService {
+    private final ClienteConverter clienteConverter;
     private final ClienteRepository clienteRepository;
 
     @Autowired
-    public ClienteService(ClienteRepository clienteRepository) {
+    public ClienteService(ClienteRepository clienteRepository, ClienteConverter clienteConverter) {
         this.clienteRepository = clienteRepository;
+        this.clienteConverter = clienteConverter;
     }
 
     public List<Cliente> clientes() {
         return clienteRepository.findAll();
+    }
+
+    public Cliente cadastrarCliente(ClienteRequest clienteRequest) {
+        Cliente cliente = clienteConverter.paraModelo(clienteRequest);
+        return clienteRepository.save(cliente);
     }
 }
