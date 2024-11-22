5aa765ef8412413d8d36ae04fa151d78e338d3ef
sadrack404 <gabrielscr2023@gmail.com>
2024-11-22 15:22:13 -0300

    feat: adicionado annotation do status http criado

--- src/main/java/com/sarlym/osmanager/api/controller/ClienteController.java
+++ src/main/java/com/sarlym/osmanager/api/controller/ClienteController.java
@@ -4,6 +4,7 @@ import com.sarlym.osmanager.api.dto.ClienteDTO;
 import com.sarlym.osmanager.api.dto.request.ClienteRequest;
 import com.sarlym.osmanager.domain.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,6 +27,7 @@ public class ClienteController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ClienteDTO cadastrarCliente(@RequestBody ClienteRequest clienteRequest){
         return clienteService.cadastrarCliente(clienteRequest);
     }
