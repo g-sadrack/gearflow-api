c0a8f3d93a60c70a03ba9576219a1d5752381344
sadrack <gabrielscr2023@gmail.com>
2024-11-24 12:15:42 -0300

    feat: criada e adicionada as exceptions

---
+++ src/main/java/com/sarlym/osmanager/domain/exception/NegocioException.java
@@ -0,0 +1,18 @@
+package com.sarlym.osmanager.domain.exception;
+
+import java.io.Serial;
+
+public class NegocioException extends RuntimeException{
+
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public NegocioException(String mensagem){
+        super(mensagem);
+    }
+
+    public NegocioException(String mensagem, Throwable causa){
+        super(mensagem, causa);
+    }
+}
