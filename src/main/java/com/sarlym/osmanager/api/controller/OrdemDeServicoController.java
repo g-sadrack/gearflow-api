aabf253bb518f9b95b9863dc14333c06fa813837
sadrack <gabrielscr2023@gmail.com>
2025-05-01 16:47:32 -0300

    feat: criado o primeiro endpoint de ordem de servico e suas funcionalidades

--- src/main/java/com/sarlym/osmanager/api/controller/OrdemDeServicoController.java
+++ src/main/java/com/sarlym/osmanager/api/controller/OrdemDeServicoController.java
@@ -1,5 +1,29 @@
 package com.sarlym.osmanager.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sarlym.osmanager.api.dto.OrdemServicoDTO;
+import com.sarlym.osmanager.api.dto.dtoconverter.OrdemServicoConverter;
+import com.sarlym.osmanager.domain.service.OrdemServicoService;
+
+@RestController
+@RequestMapping(value = "api/ordens-servico")
 public class OrdemDeServicoController {
 
+    private OrdemServicoService ordemServicoService;
+    private OrdemServicoConverter ordemServicoConverter;
+
+    public OrdemDeServicoController(OrdemServicoService ordemServicoService,
+            OrdemServicoConverter ordemServicoConverter) {
+        this.ordemServicoService = ordemServicoService;
+        this.ordemServicoConverter = ordemServicoConverter;
+    }
+
+    @GetMapping("/{id}")
+    public OrdemServicoDTO buOrdemServico(@PathVariable Long id) {
+        return ordemServicoConverter.paraDto(ordemServicoService.buscaOrdemServicoOuErro(id));
+    }
 }
---
+++ src/main/java/com/sarlym/osmanager/api/dto/OrdemServicoDTO.java
@@ -0,0 +1,30 @@
+package com.sarlym.osmanager.api.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.sarlym.osmanager.api.core.enums.Status;
+import com.sarlym.osmanager.domain.model.ItemPeca;
+import com.sarlym.osmanager.domain.model.ItemServico;
+
+
+import lombok.Data;
+
+@Data
+public class OrdemServicoDTO {
+    private Long id;
+    private String numero_os;
+    private Status status;
+    private String descricaoProblema;
+    private String diagnosticoTecnico;
+    private BigDecimal valorTotal;
+    private LocalDateTime dataAbertura;
+    private LocalDateTime dataAlteracao;
+    private LocalDateTime dataFinalizacao;
+    private Boolean ativo;
+    private VeiculoDTO veiculo;
+    private MecanicoDTO mecanico;
+    private List<ItemServico> servicos;
+    private List<ItemPeca> pecas;
+}
