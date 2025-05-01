4e53706ba2f322706064706eecf0785cf8e896ab
sadrack <gabrielscr2023@gmail.com>
2025-05-01 16:43:52 -0300

    feat: adicionado o relacionamento entre ordemServico e peca

--- src/main/java/com/sarlym/osmanager/domain/model/Peca.java
+++ src/main/java/com/sarlym/osmanager/domain/model/Peca.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,5 +26,7 @@ public class Peca {
     private BigDecimal valorUnitario;
     private Integer quantidadeEstoque;
     private Integer quantidadeEstoqueMinimo;
-
+    @ManyToOne
+    @JoinColumn(name = "ordem_servico")
+    private OrdemServico ordemServico;
 }
