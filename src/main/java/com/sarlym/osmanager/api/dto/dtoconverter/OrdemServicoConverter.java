913c5b4a388c3e88edcec7ced7bcdebeb637e452
g-sadrack <gabrielscr2023@gmail.com>
2025-05-02 18:59:33 -0300

    feat: adicionado o get de busca filtrada

--- src/main/java/com/sarlym/osmanager/api/dto/dtoconverter/OrdemServicoConverter.java
+++ src/main/java/com/sarlym/osmanager/api/dto/dtoconverter/OrdemServicoConverter.java
@@ -1,5 +1,7 @@
 package com.sarlym.osmanager.api.dto.dtoconverter;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,5 +17,9 @@ public class OrdemServicoConverter {
 
     public OrdemServicoDTO paraDto (OrdemServico ordemServico){
         return modelMapper.map(ordemServico, OrdemServicoDTO.class);
+    }
+
+    public List<OrdemServicoDTO> paraListaDTO(List<OrdemServico> ordens) {
+        return ordens.stream().map(this::paraDto).toList();
     }
 }
