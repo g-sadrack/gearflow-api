1f0e412e00a645e4d8198862c7588386086c3b6d
g-sadrack <gabrielscr2023@gmail.com>
2025-05-02 18:59:03 -0300

    feat: criado o metodo para busca filtrando numero da OS, status, id do veiculo, data de inicio e datafinal

--- src/main/java/com/sarlym/osmanager/api/controller/OrdemDeServicoController.java
+++ src/main/java/com/sarlym/osmanager/api/controller/OrdemDeServicoController.java
@@ -1,12 +1,21 @@
 package com.sarlym.osmanager.api.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sarlym.osmanager.api.core.enums.Status;
 import com.sarlym.osmanager.api.dto.OrdemServicoDTO;
 import com.sarlym.osmanager.api.dto.dtoconverter.OrdemServicoConverter;
+import com.sarlym.osmanager.domain.model.OrdemServico;
 import com.sarlym.osmanager.domain.service.OrdemServicoService;
 
 @RestController
@@ -25,5 +34,16 @@ public class OrdemDeServicoController {
     @GetMapping("/{id}")
     public OrdemServicoDTO buOrdemServico(@PathVariable Long id) {
         return ordemServicoConverter.paraDto(ordemServicoService.buscaOrdemServicoOuErro(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrdemServicoDTO>> buscaFiltrada(
+            @RequestParam(required = false) String numeroOs,
+            @RequestParam(required = false) Status status,
+            @RequestParam(required = false) Long veiculoId,
+            @RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime dataInicio,
+            @RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime dataFim) {
+        List<OrdemServico> ordens = ordemServicoService.buscaComFiltros(numeroOs, status, veiculoId, dataInicio, dataFim);
+        return ResponseEntity.ok(ordemServicoConverter.paraListaDTO(ordens));
     }
 }
