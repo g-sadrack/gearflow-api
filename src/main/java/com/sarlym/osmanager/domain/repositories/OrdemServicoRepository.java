61fb351221ffe695edebb147902c513f73ef6378
sadrack <gabrielscr2023@gmail.com>
2025-06-05 09:50:45 -0300

    fix: adicionado annontation de nonnull para metodos e parametros necessarios

--- src/main/java/com/sarlym/osmanager/domain/repositories/OrdemServicoRepository.java
+++ src/main/java/com/sarlym/osmanager/domain/repositories/OrdemServicoRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import com.sarlym.osmanager.domain.model.OrdemServico;
 
-import io.micrometer.common.lang.NonNull;
 
 @Repository
 public interface OrdemServicoRepository extends JpaRepository<OrdemServico, Long>, OrdemServicoRepositoryQuerys {
@@ -37,6 +37,6 @@ public interface OrdemServicoRepository extends JpaRepository<OrdemServico, Long
     })
     @Override
     @NonNull
-    public Optional<OrdemServico> findById(Long id);
+    public Optional<OrdemServico> findById(@NonNull Long id);
 
 }
