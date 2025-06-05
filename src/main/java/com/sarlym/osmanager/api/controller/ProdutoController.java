78705145f6507bc5c99f253176dac6558ee5cf4a
sadrack <gabrielscr2023@gmail.com>
2025-06-05 13:46:44 -0300

    feat: criado o endpoint da alteracao do produto

--- src/main/java/com/sarlym/osmanager/api/controller/ProdutoController.java
+++ src/main/java/com/sarlym/osmanager/api/controller/ProdutoController.java
@@ -2,19 +2,21 @@ package com.sarlym.osmanager.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sarlym.osmanager.api.dto.mapper.ProdutoMapper;
 import com.sarlym.osmanager.api.dto.request.ProdutoRequest;
 import com.sarlym.osmanager.api.dto.response.ProdutoDTO;
 import com.sarlym.osmanager.domain.service.ProdutoService;
-
-
 
 @RestController
 @RequestMapping(value = "api/produtos", produces = { "application/json" })
@@ -29,18 +31,25 @@ public class ProdutoController {
     }
 
     @GetMapping("/{id}")
-    public ProdutoDTO buscaProduto(@PathVariable(name = "id") Long id){
+    public ProdutoDTO buscaProduto(@PathVariable(name = "id") Long id) {
         return produtoMapper.modelParaDTO(produtoService.buscaProdutoOuErro(id));
     }
 
     @GetMapping
-    public List<ProdutoDTO> listarProduto(){
+    public List<ProdutoDTO> listarProduto() {
         return produtoMapper.modeloParaDTOLista(produtoService.listaProdutos());
     }
 
     @PostMapping
-    public ProdutoDTO cadastrarProduto(@RequestBody(required = true) ProdutoRequest produtoRequest){
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProdutoDTO cadastrarProduto(@RequestBody(required = true) ProdutoRequest produtoRequest) {
         return produtoMapper.modelParaDTO(produtoService.salvar(produtoRequest));
+    }
+
+    @PutMapping("/{id}")
+    public ProdutoDTO alterarProduto(@PathVariable(name = "id") Long id,
+            @RequestBody(required = true) ProdutoRequest produtoRequest) {
+        return produtoMapper.modelParaDTO(produtoService.atualizarProduto(id, produtoRequest));
     }
 
 }
