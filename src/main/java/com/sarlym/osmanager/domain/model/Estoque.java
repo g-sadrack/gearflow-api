4fa439d87bc4db6636200ddc986e0caedb45f26e
sadrack <gabrielscr2023@gmail.com>
2025-06-14 11:33:56 -0300

    refact: anotacao de enum string

--- src/main/java/com/sarlym/osmanager/domain/model/Estoque.java
+++ src/main/java/com/sarlym/osmanager/domain/model/Estoque.java
@@ -9,6 +9,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 import com.sarlym.osmanager.api.core.enums.StatusEstoque;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -44,6 +46,7 @@ public class Estoque {
     private LocalDateTime dataUltimaEntrada;
     private LocalDateTime dataUltimaSaida;
     private String localizacao;
+    @Enumerated(EnumType.STRING)
     private StatusEstoque StatusEstoque;
     private String lote;
     @CreationTimestamp
